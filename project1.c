08f35f7632aa235e012d21701f7186d722b3c6cb
a <ajs476@nau.edu>
2016-09-15 18:40:13 -0700

    refactored code, now getting all header info correctly

--- project1.c
+++ project1.c
@@ -1,96 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-	// create file pointer to open file into
-    FILE *fp;
-    // file pointer for new output file
-    FILE *fpOut;
 
-    // initialization of variables
-    int character1;
-    int character2;
-    int character3;
-    int i;
-    int sz;
-    int sz2;
-    unsigned char red;
+FILE *fp;
+
+int tempChar;
+int i;
+int imageSize;
+int sz2;
+int imageDimensionX;
+int imageDimensionY;
+int imageDimensionColor;
+char magicNumber[2];
+
+typedef struct Pixel{
+	unsigned char red;
     unsigned char green;
     unsigned char blue;
-
-    // open up our files
-    fp = fopen("testImageAscii.ppm", "r");
-    fpOut = fopen("out.ppm", "wb");
-
-    //get size of file inputted
-    fseek(fp, 0L, SEEK_END);
-	sz = ftell(fp);
-	fseek(fp, 0L, SEEK_SET);
+    } Pixel;
 
 
-	// create char array to store file contents into
-	char *buffer = malloc(sz + 1);
-	char *buffer2 = malloc(sz + 1);
-	printf("Size of file: %i \n", sz);
 
-    if(fp == NULL){
-   		printf("Error reading file -> Empty \n");
+
+
+void skipComments(){
+	if(tempChar == '#'){
+		// comment exists, lets get to the end of it
+		printf("Comment found.. \n");
+		while(tempChar != '\n'){
+			tempChar = fgetc(fp);
+		}
+		// is the next line another comment?
+		tempChar = fgetc(fp);
+		skipComments();
+	}
+	else{
+		// get image dimensions
+		printf("End of comments.. \n");
+		// reset our file pointer to the correct position
+		fseek(fp, -2, SEEK_CUR);
+	}
+	
+}
+
+void getDimensions(){
+	// get image width
+	fscanf(fp, "%d",  &imageDimensionX);
+	printf("Image Dimensions X: %d\n", imageDimensionX);
+
+	// get image height
+	fscanf(fp, "%d\n",  &imageDimensionY);
+	printf("Image Dimensions Y: %d\n", imageDimensionY);
+}
+
+void getColorRange(){
+	// get image color range
+	fscanf(fp, "%d\n",  &imageDimensionColor);
+	printf("Image Color Range: %d\n", imageDimensionColor);
+}
+
+void getFileType(){
+	char tempBuffer[64];
+	fscanf(fp, "%s\n",  tempBuffer);
+	strcpy(magicNumber, tempBuffer);
+	printf("Magic Number: %s\n", magicNumber);
+	tempChar = fgetc(fp);
+}
+
+void getHeaderInfo(){
+	getFileType();
+	skipComments();
+	getDimensions();
+	getColorRange();	
+}
+
+int main() {
+	
+    // open up our image fo reading
+    fp = fopen("testImageAscii1.ppm", "r");
+
+	// make sure file opened exists
+	if(fp == NULL ){
+   		fprintf(stderr, "Error reading file... \n");
    		return 1;
     }
-    else{
-   		printf("Success: File opened \n");
-   		character1 = fgetc(fp);
-   		buffer[0] = character1;
-   		character2 = fgetc(fp);
-   		buffer[0] = character2;
-   		character3 = fgetc(fp);
-   	if(character1 == 80){
-   		if(character2 == 51){
-   			// checking if next character is on a new line
-   			if(character3 == '\n'){
-   				// newline was found to be the next char
-   				printf("New line detected \n");
-				character3 = fgetc(fp);
-				printf("Current char: %d \n", character3);
-   			}
-   			else{
-   				// char 3 was not a newline, display it for testing
-   				printf("char3: %c \n", character3);
-   			}
-   			// file is P3
-   			printf("Filetype: P3 \n");
-   			// loop through file and copy contents into buffer
-   			for(i = 3; i < sz-3; i++){
-   				character1 = fgetc(fp);
-   				buffer[i] = character1;
-   			}
-   			// display buffer contents in window
-   			// fwrite(buffer, sz, 1, stdout);
-   		}
-   		else if(character2 == 54){
-   			// file is P4
-   			printf("Filetype: P4 \n");
-   			// read p4 file into buffer
-   			fread(buffer2, sz, 1, fp);
-   		    // write p4 file into new out file
-   			fwrite(buffer2, sz, 1, fpOut);
-   		}
-   	else{
-   		// file is not P3 or P4, error
-   		printf("Error: unsupported filetype \n");
-   	}
-   		
-   		
-   	}
-   	else{
-   		// first character in header was 'P', but not 3 or 4 following found
-   		printf("Error: cannot continue, unsupported filetype \n");
-   	}
-   	
-   }
-   
-   fclose(fp);
-   fclose(fpOut);
 
-   return 0;
+  	printf("Success: File opened \n");
+
+	//get size of image
+    fseek(fp, 0L, SEEK_END);
+	imageSize = ftell(fp);
+	rewind(fp);
+
+	printf("Size of file: %i \n", imageSize);
+   
+ 
+	// create char array to store file contents into
+	char *imageBuffer = malloc(imageSize + 1);
+	int i = 0;
+	while(!feof(fp)){
+		imageBuffer[i] = fgetc(fp);
+		i++;
+	}
+	imageBuffer[i] = 0;
+
+	// printing contents of imageBuffer
+	/*int j = 0;
+	while(j < .0005*i){
+		printf("%c", imageBuffer[j]);
+		j++;
+	}
+	*/
+	
+	// reset file pointer position to beginning of file
+	rewind(fp);
+
+	getHeaderInfo();
+
+
+	
+	
+
+
+
+
+
+	
+
+
+	fclose(fp);
+
+    return 0;
 }
